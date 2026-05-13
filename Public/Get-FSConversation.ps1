e647bb1eb9d551096c571bda587a7fb9ba518989
Hugh Fletcher <hughafletcher@gmail.com>
2026-05-13 16:03:57 -0500

    Add -OutFile parameter to Get-FSConversation
    When specified, writes the full conversation as JSON (UTF-8) to the given
    file path instead of returning an object to the pipeline. Creates parent
    directories if they don't exist.
    
    Fixes #5

--- Public/Get-FSConversation.ps1
+++ Public/Get-FSConversation.ps1
@@ -13,18 +13,27 @@ function Get-FSConversation {
         Comma-separated list of related data to include: threads, timelogs, tags.
         Defaults to "threads".
 
+    .PARAMETER OutFile
+        Write the full conversation as JSON (UTF-8) to this file path instead of
+        returning an object to the pipeline. Creates parent directories if needed.
+
     .EXAMPLE
         Get-FSConversation -Id 17064
 
     .EXAMPLE
         Get-FSConversation -Id 17064 -Embed "threads,tags"
+
+    .EXAMPLE
+        Get-FSConversation -Id 17064 -Embed "threads,tags" -OutFile "C:\temp\ticket-17064.json"
     #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [int]$Id,
 
-        [string]$Embed = "threads"
+        [string]$Embed = "threads",
+
+        [string]$OutFile
     )
 
     if (-not $script:FSConfig) {
@@ -39,5 +48,16 @@ function Get-FSConversation {
     }
 
     $response = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
-    return $response
+
+    if ($OutFile) {
+        $parent = Split-Path -Path $OutFile -Parent
+        if ($parent -and -not (Test-Path $parent)) {
+            New-Item -Path $parent -ItemType Directory -Force | Out-Null
+        }
+        $response | ConvertTo-Json -Depth 10 | Set-Content -Path $OutFile -Encoding UTF8
+        Write-Verbose "Conversation $Id written to $OutFile"
+    }
+    else {
+        return $response
+    }
 }
